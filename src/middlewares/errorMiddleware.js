fb3d479934ac185e715d7c1f1643adac81b9ee1f
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-05-01 13:17:31 +0100

    error handlers

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,42 @@
+import { ValidationError, BaseError } from "sequelize";
+
+export const badRequestHandler = (err, req, res, next) => {
+    if (
+        err.status === 400 ||
+        err instanceof ValidationError ||
+        err instanceof BaseError
+    ) {
+        res.status(400).send({ message: err.message });
+    } else {
+        next(err);
+    }
+};
+
+export const unauthorizedHandler = (err, req, res, next) => {
+    if (err.status === 401) {
+        res.status(401).send({ success: false, message: err.message });
+    } else {
+        next(err);
+    }
+};
+
+export const forbiddenErrorHandler = (err, req, res, next) => {
+    if (err.status === 403) {
+        res.status(403).send({ success: false, message: err.message });
+    } else {
+        next(err);
+    }
+};
+
+export const notfoundHandler = (err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404).send({ success: false, message: err.message });
+    } else {
+        next(err);
+    }
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ message: "We gonna fix this ASAP!" });
+};
